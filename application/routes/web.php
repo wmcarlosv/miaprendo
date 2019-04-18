815899c7c91b4cf52c53be33f1766561ba6c1b76
Carlos Vargas <cvargas@frontuari.com>
2019-04-17 21:49:18 -0430

    agregando seguridad

--- application/routes/web.php
+++ application/routes/web.php
@@ -19,7 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'admin'], function(){
+Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function(){
 
 	Route::resource('lessons','LessonsController');
 	Route::resource('calendars','CalendarsController');
