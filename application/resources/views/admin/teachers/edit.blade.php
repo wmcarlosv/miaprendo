323153179cce066394e260af41c9d65f091455f0
= <=>
2019-05-03 13:48:50 -0430

    agregando datos

--- application/resources/views/admin/teachers/edit.blade.php
+++ application/resources/views/admin/teachers/edit.blade.php
@@ -47,6 +47,27 @@
             {!! Form::close() !!}
     	</div>
     </div>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h2>Cambiar Contrase&ntilde;a</h2>
+        </div>
+        <div class="panel-body">
+            {!! Form::open(['route' => ['change_password','id' => $teacher->id], 'method' => 'PUT', 'autocomplete' => 'off']) !!}
+            <div class="form-group">
+                <label for="password">Nueva Contrase&ntilde;a: </label>
+                <input type="password" class="form-control" name="password" id="password" />
+            </div>
+
+            <div class="form-group">
+                <label for="password_confirmation">Repita Contrase&ntilde;a: </label>
+                <input type="password" class="form-control" name="password_confirmation" id="password_confirmation" />
+            </div>
+            <button type="sumit" class="btn btn-success">Guardar</button>
+            <a href="{{ route('home') }}" class="btn btn-danger">Cancelar</a>
+            {!! Form::close() !!}
+        </div>
+    </div>
 @stop
 @section('js')
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
